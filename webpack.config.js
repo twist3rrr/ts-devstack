2db90596894e58c51e45779a051f23389a2e6513
Roman Pylyp <twist3r@Roman-MacBook-Pro.local>
2018-12-15 20:15:42 +0100

    typescript setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    mode: 'development',
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    }
+}
